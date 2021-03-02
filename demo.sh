623d6f980e08ef2571b3e83d333a48e10f7a8996
Daniel Schimpfoessl <daniel.schimpfoessl@redhat.com>
2021-03-02 10:33:38 -0600

    Refine demo

--- demo.sh
+++ demo.sh
@@ -17,18 +17,23 @@ cmd jq \'.history[].created_by\' b4f59ea05ae49a910caeffa65896bfbc53040fb4d480a01
 ___ "Inspect Image Layer"
 cmd tar -tf eb7bf34352ca9ba2fb0218870ac3c47b76d0b1fb7d50543d3ecfa497eca242b0.tar
 cmd tar -tf 5cbd821b0b9630e7fca368313ba65143e9ab442da663a0ac30ce17b0a968befc.tar
-___ "Graph Driver"
-cmd podman info
 
 __ "Run Container"
 ___ "Create isolated process"
-cmd podman run -d --rm demo/chrome-rdp bash -c \'tail -f /dev/null\'
+cmd podman run -d --cidfile container-id --rm demo/chrome-rdp tail -f /dev/null
 pid=$(ps -efl | grep null | grep -v grep | awk '{ print $4; }')
 echo $pid
+___ "Host process tree"
 cmd pstree -spc $pid
+___ "Container process tree"
+podman exec -it $(cat container-id) ls -l /proc/1/cmdline
+podman exec -it $(cat container-id) cat /proc/1/cmdline
+echo
 ___ "List container"
 cmd podman ps
 ___ "Kill container process"
 cmd kill -9 $pid
 ___ "List container"
 cmd podman ps
+___ "Container Runtime Components"
+cmd podman info
